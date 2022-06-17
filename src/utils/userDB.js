26512dc7a603735c7a32b2fed7a008a18e9c257d
Carlos Dominguez <litosandres@hotmail.com>
2022-06-16 20:23:16 -0600

    agregamos los componentes LoginForm y UserData de Acount

---
+++ src/utils/userDB.js
@@ -0,0 +1,11 @@
+export const user = {
+    username: "xagustin93",
+    password: "1234567890"
+}
+
+export const userDetails = {
+    username: "xagustin93",
+    firstName: "Agustin",
+    lastName: "Navarro Galdon",
+    email: "aguston@gmail.com"
+}
