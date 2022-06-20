aa97207b88c3c28068a2de1d1b623d9ff81bbe35
Carlos Dominguez <litosandres@hotmail.com>
2022-06-20 13:36:38 -0600

    agregamos el Screen de Pokemon en el componente FavoritesNavigation en navigation para que cuando vemos el detalle de un pokemon en favoritos no nos regrese al pokedex

--- src/navigation/FavoritesNavigation.js
+++ src/navigation/FavoritesNavigation.js
@@ -2,15 +2,25 @@ import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 
 import Favorites from '../screens/Favorites'
-
+import Pokemon from "../screens/Pokemon"
 const Stack = createStackNavigator()
 
 export default function FavoritesNavigation() {
   return (
     <Stack.Navigator>
-        <Stack.Screen name="Favorites"  component={Favorites} options={{
+        <Stack.Screen 
+        name="Favorites"  
+        component={Favorites} 
+        options={{
             title: "Favoritos"
         }} />
+        <Stack.Screen name="Pokemon" 
+        component={Pokemon}
+        options={{
+          title:"",
+          headerTransparent: true
+        }}
+        />
     </Stack.Navigator>
   )
 }
