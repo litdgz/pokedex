cb8e41d4c50f227bff68c36958ae66767d7a9ef4
Carlos Dominguez <litosandres@hotmail.com>
2022-06-20 16:53:52 -0600

    hacemos la configuracion basica antes del BUILD

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -13,7 +13,7 @@ const Tab = createBottomTabNavigator()
 
 export default function Navigation() {
     return(
-        <Tab.Navigator>
+        <Tab.Navigator initialRouteName="Pokedex">
             <Tab.Screen name="Favorites" component={FavoritesNavigation} options={{
                 tabBarLabel: "Favoritos",
                 tabBarIcon: ({color, size}) => (
