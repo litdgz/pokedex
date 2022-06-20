6eecb18932824057296b6579572b487b6179f961
Carlos Dominguez <litosandres@hotmail.com>
2022-06-20 16:42:48 -0600

    agregamos la cantidad de favoritos que tenemos en nuestra cuenta

--- src/components/Auth/UserData.js
+++ src/components/Auth/UserData.js
@@ -1,10 +1,29 @@
 import { View, Text, StyleSheet, Button } from 'react-native'
-import React from 'react'
+import React, { useCallback, useState} from 'react'
+import { useFocusEffect } from '@react-navigation/native'
+import { size } from 'lodash'
 
+import { getPokemonsFavoriteApi } from "../../api/favorite"
 import useAuth from "../../hooks/useAuth"
 
 export default function UserData() {
     const { auth, logout } = useAuth()
+    const [total, setTotal] = useState(0)
+
+    useFocusEffect(
+        useCallback(() => {
+            (async () => {
+                try {
+                    const response = await getPokemonsFavoriteApi();
+                    setTotal(size(response))
+                } catch (error) {
+                    setTotal(0)
+                }
+            })()
+        })
+    )
+
+
   return (
     <View style={styles.content}>
       <View style={styles.titleBlock}>
@@ -18,7 +37,7 @@ export default function UserData() {
         <ItemMenu title="Nombre" text={`${auth.firstName} ${auth.lastName}`}/>
         <ItemMenu title="Username" text={auth.username}/>
         <ItemMenu title="Email" text={auth.email}/>
-        <ItemMenu title="Total Favoritos" text={`0 pokemons`}/>
+        <ItemMenu title="Total Favoritos" text={`${total} pokemons`}/>
 
       </View>
 
